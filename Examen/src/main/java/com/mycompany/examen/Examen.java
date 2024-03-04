9427f8d013ee471f18e59d169f4265579bb08f4b
Wilber Ordonez <wordonezr2@miumg.edu.gt>
2024-03-04 10:19:10 -0600

    primera prueba para ver si funciona

--- Examen/src/main/java/com/mycompany/examen/Examen.java
+++ Examen/src/main/java/com/mycompany/examen/Examen.java
@@ -11,6 +11,6 @@ package com.mycompany.examen;
 public class Examen {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Prueba!");
     }
 }
